a649ed66be2f22a850de6561e6e3bb372f778d24
faraztayari <faraztayari@yahoo.com>
2025-11-30 15:13:47 +0330

    change BG Pcolor

--- tailwind.config.js
+++ tailwind.config.js
@@ -67,7 +67,7 @@ module.exports = {
         // #000814 black
         'primary': '#ffc300',
         'gold': '#ffd60a',
-        'dark': '#001413',
+        'dark': '#555555ff',
         'dark-alt': '#012f38',
         'dark-secondary': '#067470c7',
         'secondary-primary': '#fcaf17',
